ebbbf26289b94501eb586944674f9817f5bba17b
steven jacobs <stjacobs@fastmail.fm>
2015-02-03 00:40:14 -0500

    removed unnecessary comments

--- historical_finance_data.py
+++ historical_finance_data.py
@@ -37,27 +37,16 @@ r = requests.get('http://ichart.yahoo.com/table.csv', params=payload)
 # construct a csv file a csv file
 
 def create_csv(ticker_symbol):
-
-	# csv files will be written in data directory
-
 	subdirectory = 'data'
-
-	# see if data directory exists. if it does not, create one.
-
 	try:
 		os.mkdir(subdirectory)
 	except Exception:
 		pass
-
-	# open/create file. name file by ticker symbol. write and close.
-
 	csv_file = ticker + '.csv'
 	
 	f = open(os.path.join(subdirectory, csv_file),'wb')
 	f.write(r.text)
 	f.close
-
-# make sure request is valid and run through create_csv(ticker)
 
 if r.status_code == 200:
 	create_csv(ticker)
