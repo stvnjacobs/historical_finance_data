a91fc84ca7520fc2df8737b1075a810e49e6487c
steven jacobs <stjacobs@fastmail.fm>
2015-06-09 21:56:36 -0400

    style fixes

--- historical_finance_data.py
+++ historical_finance_data.py
@@ -13,11 +13,11 @@ def create_directory():
         pass
 
 def create_csv(ticker, response):
-	csv_file = ticker + '.csv'
+    csv_file = ticker + '.csv'
 
-	f = open(os.path.join(subdirectory, csv_file),'wb')
-	f.write(response)
-	f.close
+    f = open(os.path.join(subdirectory, csv_file),'wb')
+    f.write(response)
+    f.close
 
 @click.command()
 @click.option('--ticker', prompt='Ticker symbol',
@@ -25,25 +25,25 @@ def create_csv(ticker, response):
 def run(ticker):
     """Simple program that outputs a csv of historical ticker data"""
     payload = {
-	's': ticker, # ticker symbol
-	'c': '',     # start year
-	'a': '',     # start month - 1
-	'b': '',     # start day
-	'f': '',     # to year
-	'd': '',     # to month - 1
+        's': ticker, # ticker symbol
+        'c': '',     # start year
+        'a': '',     # start month - 1
+        'b': '',     # start day
+        'f': '',     # to year
+        'd': '',     # to month - 1
         'e': '',     # to day
         'g': 'd'     # interval
-	               # d = daily
-                   # w = weekly
-                   # m = monthly
-                   # v = dividends only
+	                # d = daily
+                        # w = weekly
+                        # m = monthly
+                        # v = dividends only
     }
 
     r = requests.get('http://ichart.yahoo.com/table.csv', params=payload)
 
     if r.status_code == 200:
         create_directory()
-	create_csv(ticker, r.text)
+        create_csv(ticker, r.text)
     else:
         click.echo("Something didn't go as planned.  Check your ticker symbol.")
 
