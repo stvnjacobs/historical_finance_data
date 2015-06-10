46f9c2a1c8caded70ed23ee29b7be0a5c0a77a5d
steven jacobs <stjacobs@fastmail.fm>
2015-06-09 21:53:47 -0400

    Move from argparse to click
    This method makes more sense to me, and I find it more easily readable.  The resulting help documentation and other helpers make it more similar to other UNIX command line programs.

--- historical_finance_data.py
+++ historical_finance_data.py
@@ -1,56 +1,51 @@
 #!/usr/bin/python
 
-import argparse
 import requests
 import os.path
+import click
 
-# command line parser
+subdirectory = 'data'
 
-parser = argparse.ArgumentParser()
-parser.add_argument("ticker", help="ticker symbol to be used")
-args = parser.parse_args()
+def create_directory():
+    try:
+        os.mkdir(subdirectory)
+    except Exception:
+        pass
 
-ticker = args.ticker
+def create_csv(ticker, response):
+	csv_file = ticker + '.csv'
 
-# load parameters into payload for request
+	f = open(os.path.join(subdirectory, csv_file),'wb')
+	f.write(response)
+	f.close
 
-payload = {
+@click.command()
+@click.option('--ticker', prompt='Ticker symbol',
+              help='Ticker symbol to look up.')
+def run(ticker):
+    """Simple program that outputs a csv of historical ticker data"""
+    payload = {
 	's': ticker, # ticker symbol
 	'c': '',     # start year
 	'a': '',     # start month - 1
 	'b': '',     # start day
 	'f': '',     # to year
 	'd': '',     # to month - 1
-	'e': '',     # to day
-	'g': 'd'     # interval
+        'e': '',     # to day
+        'g': 'd'     # interval
 	               # d = daily
                    # w = weekly
                    # m = monthly
                    # v = dividends only
-}
+    }
 
-# set request
+    r = requests.get('http://ichart.yahoo.com/table.csv', params=payload)
 
-r = requests.get('http://ichart.yahoo.com/table.csv', params=payload)
+    if r.status_code == 200:
+        create_directory()
+	create_csv(ticker, r.text)
+    else:
+        click.echo("Something didn't go as planned.  Check your ticker symbol.")
 
-
-# construct a csv file a csv file
-
-def create_csv(ticker_symbol):
-	subdirectory = 'data'
-	try:
-		os.mkdir(subdirectory)
-	except Exception:
-		pass
-	csv_file = ticker + '.csv'
-	
-	f = open(os.path.join(subdirectory, csv_file),'wb')
-	f.write(r.text)
-	f.close
-
-if r.status_code == 200:
-	create_csv(ticker)
-else:
-	print "Something didn't go as planned.  Check your ticker symbol."
-
-
+if __name__ == '__main__':
+    run()
